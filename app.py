acb50560ea682ac45f8f9979aa1fa434d4a9979d
Aditya Prakash Singh <aditya@Adityas-MacBook-Pro.local>
2023-06-29 09:34:06 +0530

    pushing code changes

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+import psutil
+from flask import Flask, render_template
+
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    return "This is a flask application"
 
+@app.route("/")
+def index():
+    cpu_percent = psutil.cpu_percent()
+    memory_percent = psutil.virtual_memory().percent
+    Message = None
+    if cpu_percent > 80 or memory_percent > 80:
+        Message = "High CPU or Memory Detected, scale up!!!"
+    return render_template("index.html", cpu_percent=cpu_percent, memory_percent=memory_percent, message=Message)
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=8001)
 
-
-
+###
+## return f'CPU usage: {cpu_percent}%'
+# 
+# @app.route('/memory')
+ #def memory():
+  #  memory_percent = psutil.virtual_memory().percent 
+   # return f'memory_utilisation: {memory_percent}%'
+   # return render_template('index.html', memory_percent= memory_percent)
+# 
+# 
+# 
+# 
+# 
+# 
+#  ###
